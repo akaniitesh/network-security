85d9dc4ac4aaf1c4c828992e063e0a07ede5c649
Nitesh Kumar <80585954+akaniitesh@users.noreply.github.com>
2026-08-02 08:57:29 +0530

    implement github action and aws s3 bucket

---
+++ networksecurity/cloud/s3_syncer.py
@@ -0,0 +1,12 @@
+
+import os
+import subprocess
+
+class S3Sync:
+    def sync_folder_to_s3(self,folder,aws_bucket_url):
+        command = f"aws s3 sync {folder} {aws_bucket_url} "
+        subprocess.run(command,shell=True,check=True)
+
+    def sync_folder_from_s3(self,folder,aws_bucket_url):
+        command = f"aws s3 sync  {aws_bucket_url} {folder} "
+        subprocess.run(command,shell=True,check=True)
